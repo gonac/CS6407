ac18e2fd23adf635a6c0067235f254ce2cfef508
Michael Murphy <mcmurphy91@gmail.com>
2015-03-31 15:30:07 +0100

    Removed unnecessary test cases

--- test/main/AllTests.java
+++ test/main/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BMSTest1.class, CarSensorTest.class,ProcessingUnitTest.class,UseCases.class })
+@SuiteClasses({ BMSTest1.class, CarSensorTest.class,ProcessingUnitTest.class})
 public class AllTests {
 
 }
